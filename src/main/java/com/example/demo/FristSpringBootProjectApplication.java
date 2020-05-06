bbce7cf391408e77e413b658542eaeabfe5502b9
JongPil Yoon <whdvlf94@gmail.com>
2020-05-06 11:07:41 +0900

    first commit for springboot project

---
+++ src/main/java/com/example/demo/FristSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FristSpringBootProjectApplication.class, args);
+    }
+
+}
